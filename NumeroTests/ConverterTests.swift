b3e2b5f2c803bccbf0a381012d93aa7cf5e5d561
Ning Ma <ning.ma@globalrelay.net>
2020-06-05 18:27:46 -0700

    Add lottie test to unit test.

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -1,5 +1,6 @@
 import XCTest
 @testable import Numero
+import Lottie
 
 class ConverterTests: XCTestCase {
   var converter: Converter!
@@ -17,5 +18,7 @@ class ConverterTests: XCTestCase {
   func testConversionForOne() {
     let result = converter.convert(1)
     XCTAssertNotEqual(result, "I", "Conversion for 1 is incorrect")
+    let animationView = AnimationView()
+    XCTAssertNotNil(animationView)
   }
 }
